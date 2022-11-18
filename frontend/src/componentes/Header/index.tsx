fd160bbb7ba8f47fee2e3710e5b17da193a76798
brayansoaresalves <brayan.iub10@gmail.com>
2022-11-17 22:23:57 -0300

    Outros Componentes

---
+++ frontend/src/componentes/Header/index.tsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/img/logo.svg'
+
+import './style.css'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={logo} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+                        <a href="https://www.instagram.com/brayan.alvess/">@Brayan Alves</a>
+                    </p>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
